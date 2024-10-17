0a76067d65795d4f8244f8c81b8b5c8885179b9c
manjur0 <sfmanjur21@gmail.com>
2024-10-17 17:27:57 +0600

    task main board html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero Section/Hero";
 import Footer from "./Components/Footer";
+import TaskBoard from "./Components/Tasks/TaskBoard";
 const App = () => {
   return (
     <div className="container mx-auto">
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </div>
   );
