969ba19e7580fcc3d02882cb254ccfe96ee7e997
manjur0 <sfmanjur21@gmail.com>
2024-10-17 12:43:33 +0600

    Tasker Project initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 
-
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
