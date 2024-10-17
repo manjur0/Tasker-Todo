12bdba949280c24f3f2cf3225cd3b471711b6f26
manjur0 <sfmanjur21@gmail.com>
2024-10-17 17:16:46 +0600

    Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero Section/Hero";
-
+import Footer from "./Components/Footer";
 const App = () => {
   return (
     <div className="container mx-auto">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
